e7ae136c9148501da44a8934851226b53e3f0d05
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2017-08-25 00:52:38 +0530

    err msg v3

--- students/views.py
+++ students/views.py
@@ -171,7 +171,7 @@ class StudentSignUp(View):
             error_msg = {}
             error_msg['traceback'] = traceback.format_exc()
             error_msg['Id'] = roll
-            send_error_email(error_msg)
+            send_error_email(str(error_msg))
             return send_400(self.response)
 
 
