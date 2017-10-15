8529b1965c8f6fea0efb414d5e7d9c990240e867
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2017-10-15 16:10:50 +0530

    Daily Data View Modified

--- DigiNehruPy/DailyData.py
+++ DigiNehruPy/DailyData.py
@@ -1,10 +1,10 @@
 from studentmeal.models import mealcount
 from students.models import Students
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
-datetime.now(pytz.utc).date()
+dt = datetime.now(pytz.utc).date()
 print "Registered Students: ", Students.objects.filter(status='1').count()
-print "Lunch: " ,mealcount.objects.filter(eating_on=datetime.now(pytz.utc).date(),meals_taken__icontains='1').count()
-print "Dinner: " ,mealcount.objects.filter(eating_on=datetime.now(pytz.utc).date(),meals_taken__icontains='3').count()
-print "Breakfast: " ,mealcount.objects.filter(eating_on=datetime.now(pytz.utc).date(),meals_taken__icontains='0').count()
-print "Snacks: " ,mealcount.objects.filter(eating_on=datetime.now(pytz.utc).date(),meals_taken__icontains='2').count()
+print "Lunch: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_taken__icontains='1').count()
+print "Dinner: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_taken__icontains='3').count()
+print "Breakfast: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_taken__icontains='0').count()
+print "Snacks: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_taken__icontains='2').count()
