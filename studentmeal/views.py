6e029b42b3ef55c63f4c37e40cd8cf1bb12a2539
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2017-10-16 01:44:02 +0530

    meal ask

--- studentmeal/views.py
+++ studentmeal/views.py
@@ -172,26 +172,47 @@ class MealCount(View):
             return send_400(self.response)
         id_ = data['id']
         time = "-1"
+        if data['time']:
+            time = data['time']
+            time1 = str((int(time) + 1) % 4)
         dated = datetime.now(pytz.utc)
         hr = datetime.strptime(str(dated.astimezone(tz.gettz('Asia/Kolkata'))),
                                '%Y-%m-%d %H:%M:%S.%f+05:30')
         hrs = hr.hour
+        nxt = 0
+
+        if data['nxt'] == '1':
+            nxt = 1
+        
         if hrs <= 10 and hrs >= 7:
             time = "0"
+            time1 = str((int(time) + 1) % 4)
         elif hrs >= 12 and hrs <= 14:
             time = "1"
+            time1 = str((int(time) + 1) % 4)
         elif hrs >= 16 and hrs <= 18:
             time = "2"
+            time1 = str((int(time) + 1) % 4)
         elif hrs >= 19 and hrs <= 21:
             time = "3"
+            time1 = str((int(time) + 1) % 4)
+
         vn_choice = data['vn']
-        vn = {}
-        vn[time] = vn_choice
+        time = "3"
+        time1 = str((int(time) + 1) % 4)
+        veg = 0
+        if vn_choice == "VE":
+            veg = 1
+        
+        # vn = {}
+        # vn[time] = vn_choice
         if time not in ["0", "1", "2", "3"]:
             self.response['res_str'] = "You are late! Mess is closed!!"
             return send_400(self.response)
         # for present day entry
         eating_on = datetime.now(pytz.utc).date()
+        import ipdb
+        ipdb.set_trace()
         try:
             st = Students.objects.get(token=id_)
             if st.status == INACTIVE:
@@ -209,13 +230,28 @@ class MealCount(View):
                     self.response['res_str'] = "Meal already taken!!"
                     return send_400(self.response)
                 eat.meals_taken.append(time)
-                eat.is_veg.append(vn_choice)
+                if veg:
+                    eat.is_veg.append(time)
+                if nxt:
+                    eat.meals_opted.append(time1)
                 eat.save()
                 self.response['res_str'] = "You can take your meal"
                 return send_200(self.response)
             else:
-                mealcount.objects.create(student=st, eating_on=eating_on,
-                                         meals_taken=time, vn=vn)
+                if nxt and time == "3":
+                    mealcount.objects.create(student=st, eating_on=eating_on + timedelta(1),
+                                             meals_opted=time1)
+                    eat = mealcount.objects.create(student=st, eating_on=eating_on,
+                                                   meals_taken=time)
+                elif nxt:
+                    eat = mealcount.objects.create(student=st, eating_on=eating_on,
+                                                   meals_taken=time, meals_opted=time1)
+                else:
+                    eat = mealcount.objects.create(student=st, eating_on=eating_on,
+                                                   meals_taken=time)
+                if veg:
+                    eat.is_veg = time
+                eat.save()
                 self.response['res_str'] = "You can take your meal"
                 return send_200(self.response)
         except Exception as e:
