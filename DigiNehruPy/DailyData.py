82e34780a3ea223e91996b9c4113ef4fcbeb1b66
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2017-10-15 16:11:13 +0530

    Daily Data View Modified

--- DigiNehruPy/DailyData.py
+++ DigiNehruPy/DailyData.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import pytz
 dt = datetime.now(pytz.utc).date()
 print "Registered Students: ", Students.objects.filter(status='1').count()
-print "Lunch: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_taken__icontains='1').count()
-print "Dinner: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_taken__icontains='3').count()
 print "Breakfast: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_taken__icontains='0').count()
+print "Lunch: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_taken__icontains='1').count()
 print "Snacks: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_taken__icontains='2').count()
+print "Dinner: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_taken__icontains='3').count()
