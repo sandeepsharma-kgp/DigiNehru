f78dc7c2d23fe14b35cd7b011f378c406ff00caf
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2017-10-16 02:23:48 +0530

    meal ask lunch and dinner only

--- studentmeal/views.py
+++ studentmeal/views.py
@@ -182,27 +182,27 @@ class MealCount(View):
 
         if data['nxt'] == '1':
             nxt = 1
-        
+
         if hrs <= 10 and hrs >= 7:
             time = "0"
             time1 = str((int(time) + 1) % 4)
         elif hrs >= 12 and hrs <= 14:
             time = "1"
-            time1 = str((int(time) + 1) % 4)
+            time1 = str((int(time) + 2) % 4)
         elif hrs >= 16 and hrs <= 18:
             time = "2"
             time1 = str((int(time) + 1) % 4)
         elif hrs >= 19 and hrs <= 21:
             time = "3"
-            time1 = str((int(time) + 1) % 4)
+            time1 = str((int(time) + 2) % 4)
 
         vn_choice = data['vn']
         time = "3"
-        time1 = str((int(time) + 1) % 4)
+        time1 = str((int(time) + 2) % 4)
         veg = 0
         if vn_choice == "VE":
             veg = 1
-        
+
         # vn = {}
         # vn[time] = vn_choice
         if time not in ["0", "1", "2", "3"]:
@@ -221,26 +221,23 @@ class MealCount(View):
                                             eating_on=eating_on)
             except:
                 eat = None
-
+            if nxt and time == "3":
+                    mealcount.objects.create(student=st, eating_on=eating_on + timedelta(1),
+                                             meals_opted=time1)
             if eat:
                 if time in eat.meals_taken:
                     self.response['res_str'] = "Meal already taken!!"
                     return send_400(self.response)
-                eat.meals_taken.append(time)
+                eat.meals_taken.append(time)    
                 if veg:
                     eat.is_veg.append(time)
-                if nxt:
+                if nxt and time != "3":
                     eat.meals_opted.append(time1)
                 eat.save()
                 self.response['res_str'] = "You can take your meal"
                 return send_200(self.response)
             else:
-                if nxt and time == "3":
-                    mealcount.objects.create(student=st, eating_on=eating_on + timedelta(1),
-                                             meals_opted=time1)
-                    eat = mealcount.objects.create(student=st, eating_on=eating_on,
-                                                   meals_taken=time)
-                elif nxt:
+                if nxt:
                     eat = mealcount.objects.create(student=st, eating_on=eating_on,
                                                    meals_taken=time, meals_opted=time1)
                 else:
