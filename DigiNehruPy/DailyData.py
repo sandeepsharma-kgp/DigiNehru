f04d181eea6b43abafb65b66c6602a65b20860f5
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2017-10-16 08:26:14 +0530

    Daily Data modified

--- DigiNehruPy/DailyData.py
+++ DigiNehruPy/DailyData.py
@@ -8,3 +8,5 @@ print "Breakfast: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals
 print "Lunch: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_taken__icontains='1').count()
 print "Snacks: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_taken__icontains='2').count()
 print "Dinner: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_taken__icontains='3').count()
+print "Next Lunch: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_opted__icontains='1').count()
+print "Next Dinner: ", mealcount.objects.filter(eating_on=dt + timedelta(0), meals_opted__icontains='3').count()
