404e9cf15a152652f4f416b0ee8c1ab37f09b9f2
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2017-10-16 06:41:57 +0530

    time 0 check

--- studentmeal/views.py
+++ studentmeal/views.py
@@ -182,7 +182,9 @@ class MealCount(View):
 
         if data['nxt'] == '1':
             nxt = 1
-
+        ##########
+        hrs = 8
+        #########
         if hrs <= 10 and hrs >= 7:
             time = "0"
             time1 = str((int(time) + 1) % 4)
@@ -196,15 +198,11 @@ class MealCount(View):
             time = "3"
             time1 = str((int(time) + 2) % 4)
 
-        vn_choice = data['vn']
-        time = "0"
-        time1 = str((int(time) + 1) % 4)
         veg = 0
+        vn_choice = data['vn']
         if vn_choice == "VE":
             veg = 1
-
-        # vn = {}
-        # vn[time] = vn_choice
+        
         if time not in ["0", "1", "2", "3"]:
             self.response['res_str'] = "You are late! Mess is closed!!"
             return send_400(self.response)
@@ -221,14 +219,15 @@ class MealCount(View):
                                             eating_on=eating_on)
             except:
                 eat = None
-            if nxt and time == "3":
-                mealcount.objects.create(student=st, eating_on=eating_on + timedelta(1),
-                                         meals_opted=time1)
+
             if eat:
                 if time in eat.meals_taken:
                     self.response['res_str'] = "Meal already taken!!"
                     return send_400(self.response)
                 eat.meals_taken.append(time)
+                if nxt and time == "3":
+                    mealcount.objects.create(student=st, eating_on=eating_on + timedelta(1),
+                                             meals_opted=time1)
                 if veg:
                     eat.is_veg.append(time)
                 if nxt and time != "3":
@@ -237,6 +236,9 @@ class MealCount(View):
                 self.response['res_str'] = "You can take your meal"
                 return send_200(self.response)
             else:
+                if nxt and time == "3":
+                    mealcount.objects.create(student=st, eating_on=eating_on + timedelta(1),
+                                             meals_opted=time1)
                 if nxt and time != "3":
                     eat = mealcount.objects.create(student=st, eating_on=eating_on,
                                                    meals_taken=time, meals_opted=time1)
